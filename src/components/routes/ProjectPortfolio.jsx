7873320346217916366ea2546ab94085be38775d
Leah Stern <leahstern@nypl.org>
2024-01-30 17:53:02 -0500

    attempt at fixing images by adding repo name to img src path

--- src/components/routes/ProjectPortfolio.jsx
+++ src/components/routes/ProjectPortfolio.jsx
@@ -10,10 +10,10 @@ export default function ProjectPortfolio() {
         <h2>My Portfolio</h2>
         <div className="gallery">
           <a href="https://project-2-library-catalog.leahstern.repl.co">
-            <img src="src/images/library.jpg" alt="numerous books on a wooden bookshelf"></img>
+            <img src="./phase-2-portfolio/src/images/library.jpg" alt="numerous books on a wooden bookshelf"></img>
           </a>
           <a href="https://project-1-todo-list.leahstern.repl.co">
-            <img src="src/images/todo.jpg" alt="scrabble tiles spelling todo above a yellow post-it note"></img>
+            <img src="./phase-2-portfolio/src/images/todo.jpg" alt="scrabble tiles spelling todo above a yellow post-it note"></img>
           </a>
         </div>
       </section>
